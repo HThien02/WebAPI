a320ba8b3447ffed8e9b8fc58f15f2b6b25ada96
HThien02 <hieuthien2k2@gmail.com>
2024-05-10 00:23:12 +0700

    Add project files.

---
+++ addProduct/Controllers/ProductController.cs
@@ -0,0 +1,99 @@
+﻿using addProduct.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace addProduct.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public static List<ProductDetail> products = new List<ProductDetail>();
+
+        [HttpGet]
+        public IActionResult getAllProduct()
+        {
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult getProductByID(string id)
+        {
+            try {
+                //LinQ [Object] Query
+                var product = products.FirstOrDefault(p => p.productID == Guid.Parse(id));
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                return Ok(product);
+            } catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult createProduct(Product product)
+        {
+            var productDetail = new ProductDetail
+            {
+                productID = Guid.NewGuid(),
+                productName = product.productName,
+                productPrice = product.productPrice,
+            };
+            products.Add(productDetail);
+            return Ok(new
+            {
+                Success = true,
+                Data = products
+            });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult editProduct(string id)
+        {
+            try
+            {
+                //LinQ [Object] Query
+                var product = products.FirstOrDefault(p => p.productID == Guid.Parse(id));
+                if (product == null)
+                {
+                    return NotFound();
+                }
+
+                if (id != product.productID.ToString())
+                {
+                    return BadRequest();
+                }
+                product.productName = product.productName;
+                product.productPrice = product.productPrice;
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult deleteProduct(string id)
+        {
+            try
+            {
+                //LinQ [Object] Query
+                var product = products.FirstOrDefault(p => p.productID == Guid.Parse(id));
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                products.Remove(product);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ addProduct/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace addProduct.Models
+{
+    public class Product
+    {
+        public string? productName { get; set; }
+        public string? productPrice { get; set; }
+
+    }
+
+    public class ProductDetail : Product
+    {
+        public Guid? productID { get; set; }
+    }
+}
